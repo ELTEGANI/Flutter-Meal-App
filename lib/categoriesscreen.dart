f31bda7cfb7bd7054f36b8ef2c1ead3e0ecfcb24
altegani mohammed <tiganimoh@gmail.com>
2021-06-13 23:10:47 +0200

    add styling and theming

--- lib/categoriesscreen.dart
+++ lib/categoriesscreen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:meal_app/dummy_data.dart';
 import './category_item.dart';
@@ -5,12 +6,16 @@ import './category_item.dart';
 class CategoriesScreen extends StatelessWidget{
    @override
   Widget build(BuildContext context) {
-    return GridView(
+    return Scaffold(
+      appBar: AppBar(title:
+      const Text('Meal App'),),
+      body: GridView(
+      padding: const EdgeInsets.all(25),
       children:DUMMY_CATEGORIES.map((catData) =>CategoryItem(catData.title,catData.color)).toList(),
       gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20),);
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20),),);
   }
 }
