8fa01afa44184fe9dbee0990637dbad7f26c55c8
altegani mohammed <tiganimoh@gmail.com>
2021-06-15 19:07:28 +0200

    add product details

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:meal_app/models/meals.dart';
-
+import '../models/meals.dart';
+import '../screens/meal_screen_detail.dart';
 
 class MealItem extends StatelessWidget {
-
+  final String id;
   final String title;
   final String imageUrl;
   final int   duration;
@@ -12,17 +12,48 @@ class MealItem extends StatelessWidget {
   final Affordability affordability;
 
   MealItem({
+    required this.id,
     required this.title,
     required this.imageUrl,
     required this.duration,
     required this.complexity,
     required this.affordability});
 
-  void selectMal(){}
+  String get complexityText{
+    switch(complexity){
+      case Complexity.Simple:
+         return 'Simple';
+      case Complexity.Challenging:
+        return 'challenging';
+      case Complexity.Hard:
+        return 'Hard';
+      default:
+        return 'Unknown';
+    }
+  }
+
+  String get affordabilityText{
+    switch(affordability){
+      case Affordability.affordable:
+        return 'affordable';
+      case Affordability.Pricey:
+        return 'Pricey';
+      case Affordability.Luxurious:
+        return 'Expensive';
+      default:
+        return 'Unknown';
+    }
+  }
+
+  void selectMal(BuildContext context){
+    Navigator.of(context).pushNamed(MealScreenDetail.routeName,arguments: id);
+  }
+
+  
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: selectMal,
+      onTap:() =>  selectMal(context),
       child: Card(shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(15)),
         elevation: 4,
@@ -33,10 +64,56 @@ class MealItem extends StatelessWidget {
               topLeft: Radius.circular(15),
               topRight: Radius.circular(15)
             ),
-            child: Image.network(imageUrl,height: 250,width:double.infinity,fit: BoxFit.cover,),
+            child: Image.network(imageUrl,height: 250,width:double.infinity,fit: BoxFit.cover,
+            ),
+            ),
+            Positioned(
+              bottom: 20,
+               right: 20,
+               child:Container(
+                 width: 290,
+                 color: Colors.black54,
+                 padding: EdgeInsets.symmetric(
+                   vertical: 5,
+                   horizontal: 20
+                 ),
+                   child: Text(
+                     title,
+                     style: TextStyle(
+                         fontSize: 26,
+                         color: Colors.white
+                     ),
+                     softWrap: true,
+                     overflow: TextOverflow.fade,
+                   )
+               )
             )
-          ],),
-        ],),
+          ],
+          ),
+          Padding(
+            padding: EdgeInsets.all(20),
+            child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+                 Row(children: <Widget>[
+                   Icon(Icons.schedule),
+                   SizedBox(width: 6),
+                   Text('$duration min')
+                 ],),
+              Row(children: <Widget>[
+                Icon(Icons.work),
+                SizedBox(width: 6),
+                Text(complexityText)
+              ],),
+              Row(children: <Widget>[
+                Icon(Icons.attach_money),
+                SizedBox(width: 6),
+                Text(affordabilityText)
+              ],),
+            ],
+          ),)
+        ],
+        ),
       ),
     );
   }
